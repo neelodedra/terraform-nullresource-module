5c7397d51bdd19240576b15bea361802f114a765
neel5150 <neeloded5150@gmail.com>
2025-04-13 11:18:44 +0530

    commented local-exec

--- numerical-action/main.tf
+++ numerical-action/main.tf
@@ -3,8 +3,8 @@ resource "null_resource" "numerical_action" {
     numerical_input = tostring(var.numerical_input)
   }
 
-  provisioner "local-exec" {
+  /*provisioner "local-exec" {
     command     = "echo 'Currently using module version 2.0.0' && echo 'Numerical input received: ${var.numerical_input}'"
     working_dir = path.module
-  }
+  }*/
 }
